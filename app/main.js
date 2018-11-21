151b2412d1bf18b25e957fb93a30c7027c1edd7e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-21 10:50:50 +0400

    done with basic rendering

--- app/main.js
+++ app/main.js
@@ -3,16 +3,17 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow = null;
 
 app.on('ready', () => {
+
 	mainWindow = new BrowserWindow({ width: 1150, height: 600, show: false});
 	mainWindow.loadFile(__dirname + '/index.html');
 
 	mainWindow.once('ready-to-show', () => {
 		mainWindow.show();
+		mainWindow.webContents.openDevTools();
 	});
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
 
-	mainWindow.webContents.openDevTools();
 });
