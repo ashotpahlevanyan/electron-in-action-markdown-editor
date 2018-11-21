16205f60979ba1d6ab67c09dbadda05b368e37c8
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-21 10:30:09 +0400

    added developer tools and fixed rendering by moving to renderer from main

--- app/main.js
+++ app/main.js
@@ -3,10 +3,16 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow = null;
 
 app.on('ready', () => {
-	mainWindow = new BrowserWindow();
+	mainWindow = new BrowserWindow({ width: 1150, height: 600, show: false});
 	mainWindow.loadFile(__dirname + '/index.html');
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+	});
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
-	})
+	});
+
+	mainWindow.webContents.openDevTools();
 });
