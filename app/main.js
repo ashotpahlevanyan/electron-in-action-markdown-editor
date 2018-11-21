f9e890cc4bad539e7f5533805188a1896f90b8c8
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-21 09:32:29 +0400

    basic structure is added

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ let mainWindow = null;
 
 app.on('ready', () => {
 	mainWindow = new BrowserWindow();
-	mainWindow.loadFile('index.html');
+	mainWindow.loadFile(__dirname + '/index.html');
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
